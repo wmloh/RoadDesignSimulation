ad9745f119d346ca173fc141391912f475d89d27
Wei Min Loh <lohwm98@hotmail.com>
2019-11-23 22:23:05 -0500

    Updated cmd object parameters in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from interface.command_loop import CommandLoop
 
 if __name__ == '__main__':
-    cmd = CommandLoop()
+    cmd = CommandLoop(guide_path='./datasets/guide.txt', legend_path='./datasets/legend.txt')
     cmd.load_files('./datasets/map1.txt', './datasets/profile1.txt', './datasets/order1.csv')
     cmd.run()
