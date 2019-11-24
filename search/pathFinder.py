ba88030cbba9919537c6f651331264738656cb06
Wei Min Loh <lohwm98@hotmail.com>
2019-11-24 17:11:22 -0500

    Added docs for PriorityQueue and PathFinder

--- search/pathFinder.py
+++ search/pathFinder.py
@@ -10,19 +10,39 @@ DIR = 4
 
 
 class PathFinder:
+    __doc__ = '''
+    Class that implements the A* search algorithm that integrates with the simulation mechanics
+    '''
 
     def __init__(self, ground, heuristic):
+        '''
+        Constructor for PathFinder
+
+        :param ground: np.ndarray - 2D array of tiles
+        :param heuristic: (Int, Int, Int, Int) => Int - cost function as a heuristic function to be minimized
+        '''
         self.ground = ground
         self.heuristic = heuristic
 
     def find_path(self, x, y, desX, desY, car):
+        '''
+        Runs the A* search algorithm using the heuristic function.
+
+        :param x: Int - current x-coordinate
+        :param y: Int - current y-coordinate
+        :param desX: Int - destination x-coordinate
+        :param desY: Int - destination y-coordinate
+        :param car: Car - reference to Car object
+        :return: deque/None - return a deque object containing directions in order if a
+                              route exists, otherwise None
+        '''
         heuristic = self.heuristic
 
         # format:             fScore,          gScore, current tile, previous tuple, direction
         #                       0                 1          2              3            4
         first_node = (heuristic(x, y, desX, desY), 0, car.cur_road, None, DEFAULT)
         open = PriorityQueue()
-        open.push(first_node)
+        open.push(first_node)  # begins with the original node
         closed = set()
 
         while not open.empty():
@@ -31,37 +51,60 @@ class PathFinder:
             closed.add(tile)
 
             curX, curY = tile.get_coord()
-            if curX == desX and curY == desY:
+            if curX == desX and curY == desY:  # destination reached!
                 return self.backtrack(node)
 
             neighbours = tile.neighbours
             for i in range(9):
                 tile_neighbour = neighbours[i]
 
+                # skip None
                 if not tile_neighbour: continue
+                # skip tiles already considered
                 if tile_neighbour in closed: continue
 
                 childX, childY = tile_neighbour.get_coord()
+                # skip if tile is not traversable (and not the destination)
                 if not tile_neighbour.traversable() and not (childX == desX and childY == desY): continue
 
-                if i > 3: i -= 1
+                if i > 3: i -= 1  # calibration due to discrepancy
 
+                # builds upon the cost from the original tile
                 gScore_child = gScore + self.get_cost(i)
+                # gets relative direction from the original tile
                 child_relX, child_relY = KEYMAPPING[i]
                 fScore_child = gScore_child + heuristic(child_relX + curX, child_relY + curY, desX, desY)
 
+                # finds results if exists
                 find_result = open.find(tile_neighbour, key=lambda t: t[CUR_TILE])
                 if find_result and gScore_child >= find_result[GSCORE]:
+                    # skips if results exists and has a higher g-score
                     continue
 
+                # push into priority queue
                 open.push((fScore_child, gScore_child, tile_neighbour, node, i))
 
+        return None  # no route can be found
+
     def get_cost(self, i):
+        '''
+        Performs a mapping of direction to costs
+
+        :param i: Int - direction
+        :return: Int - cost for the direction
+        '''
         if i == NW or i == NE or i == SW or i == SE:
             return DIAG_COST
         return ORTHO_COST
 
     def backtrack(self, node):
+        '''
+        Recursively traces back starting from the initial end node that reached the destination
+        to obtain a linear forward order of directions to take.
+
+        :param node: tuple(Int, Int, Traversable, tuple(node)/None, Int) - final node containing all previous nodes
+        :return: deque - ordered queue of directions to take
+        '''
         directions = deque()
 
         directions.append(node[PREV_TUP])
@@ -70,6 +113,6 @@ class PathFinder:
             node = node[PREV_TUP]
 
         directions.reverse()
-        directions.pop()
+        directions.pop()  # removes the base case recursion
 
         return directions
--- utils/priority_queue.py
+++ utils/priority_queue.py
@@ -1,37 +1,59 @@
 class PriorityQueue:
     __doc__ = '''
-    Class for a priority queue with customizations for the A* search algorithm (using heaps)
+    Class for a min priority queue with customizations for the A* search algorithm (using heaps)
     
     Time complexity:
     * push - O(log n)
     * pop - O(log n)
+    
+    Note: Does not have type-checking functionality
     '''
 
     def __init__(self, key=lambda x: x[0]):
+        '''
+        Constructor for PriorityQueue
+
+        :param key: (tuple(...)) => Int - indexing function as key
+        '''
         self.container = list()
         self.key = key
 
     def push(self, obj):
+        '''
+        Pushes an object with a priority field and organizes appropriates
+
+        :param obj: tuple(...) - object to store with priority field
+        :return: None
+        '''
         container = self.container
         key = self.key
 
         container.append(obj)
         length = len(container)
         k = length - 1
+
+        # performs fix up
         while (k - 1) // 2 >= 0 and key(container[(k - 1) // 2]) > key(container[k]):
             parent_idx = (k - 1) // 2
             container[k], container[parent_idx] = container[parent_idx], container[k]
             k = parent_idx
 
     def pop(self):
+        '''
+        Pops and returns the root of the priority queue which has the lowest priority value.
+
+        :return: <OBJECT_TYPE> - object stored with lowest priority value
+        '''
         container = self.container
         key = self.key
 
         container[0], container[-1] = container[-1], container[0]
-        obj = container.pop()
+        obj = container.pop()  # temporarily stores the object
 
         length = len(container)
         k = 0
+
+        # performs fix down
         while 2 * k + 1 < length:
             j = 2 * k + 1
 
@@ -39,6 +61,7 @@ class PriorityQueue:
                 j += 1
 
             if key(container[j]) >= key(container[k]):
+                # guaranteed that invariant is preserved
                 break
 
             container[j], container[k] = container[k], container[j]
@@ -47,12 +70,24 @@ class PriorityQueue:
         return obj
 
     def find(self, item, key=lambda x: x[1]):
+        '''
+        Searches for an item in the priority queue using the given key
+
+        :param item: <FIND_OBJECT_TYPE> - object to find
+        :param key: (<OBJECT_TYPE>) => <FIND_OBJECT_TYPE> - key to index
+        :return: <FIND_OBJECT_TYPE>/None - returns the object if found otherwise None
+        '''
         for ele in self.container:
             if key(ele) == item:
                 return ele
         return None
 
     def empty(self):
+        '''
+        Function to check if empty for convenience
+
+        :return: Boolean - returns True if and only if empty
+        '''
         return len(self.container) == 0
 
     def __iter__(self):
