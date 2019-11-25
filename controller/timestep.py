302c0f288eace09b21a9ccef5d7e241cdf47dbdf
Wei Min Loh <lohwm98@hotmail.com>
2019-11-24 20:02:55 -0500

    Added docs for Timestep and Waiting

--- controller/timestep.py
+++ controller/timestep.py
@@ -2,18 +2,43 @@ from utils.constants import BLOCKED, REACHED
 
 
 class Timestep:
+    __doc__ = '''
+    Class that handles the sequential movement of every car active in the simulation
+    '''
 
     def __init__(self, simulation):
+        '''
+        Constructor for Timestep
+
+        :param simulation: Simulation - reference to simulation object
+        '''
         self.simulation = simulation
         self.cars = list()
 
     def attach(self, car_reference):
+        '''
+        Appends a reference to the active car that has recently turned active.
+
+        :param car_reference: Car - reference to the car
+        :return: None
+        '''
         self.cars.append(car_reference)
 
     def detach(self, car_reference):
+        '''
+        Removes a car from Timestep (turns it inactive)
+
+        :param car_reference: Car - reference to the car to be removed
+        :return: None
+        '''
         self.cars.remove(car_reference)
 
     def notify_observer(self):
+        '''
+        Moves every car sequentially depending on the order it was attached.
+
+        :return: None
+        '''
         for car in self.cars:
             state = car.move()
             if state == BLOCKED:
@@ -22,6 +47,11 @@ class Timestep:
                 self.detach(car)
 
     def get_size(self):
+        '''
+        Gets the number of active cars for convenience.
+
+        :return: Int - number of active cars
+        '''
         return len(self.cars)
 
     def __str__(self):
--- controller/waiting.py
+++ controller/waiting.py
@@ -2,10 +2,22 @@ import warnings
 from search.pathFinder import PathFinder
 from utils.constants import cost_function
 
+TIMESTEP = 0
+HOME = 3
+HUB = 4
+
 
 class Waiting:
+    __doc__ = '''
+    Class that handles and organizes cars that are not actively moving in the simulation
+    '''
 
     def __init__(self, simulation):
+        '''
+        Constructor for Waiting
+
+        :param simulation: Simulation - reference to simulation object
+        '''
         self.homes = list()
         self.backup = None
         self.simulation = simulation
@@ -13,23 +25,56 @@ class Waiting:
         self.pathfinder = PathFinder(simulation.ground, cost_function)
 
     def attach(self, ts, desX, desY, home, hub):
+        '''
+        Appends a predefined-structured tuple that represents data from the order text file
+        which indicates the order and time to move each car.
+
+        :param ts: Int - timestep to begin moving a car
+        :param desX: Int - destination x-coordinate
+        :param desY: Int - destination y-coordinate
+        :param home: Home - reference to the starting Home of the car
+        :param hub: Hub - reference to the destination Hub of the car
+        :return: None
+        '''
         if not self.fixed:
             self.homes.append((ts, desX, desY, home, hub))
         else:
             warnings.warn('Attempted to attach object to Waiting after fixing')
 
     def detach(self):
+        '''
+        Removes the last object in homes.
+
+        :return: Home/None - Home object of tuple that is removed
+        '''
         if self.fixed:
-            return self.homes.pop()[3]
+            return self.homes.pop()[3]  # TODO: Enable feature to safely remove objects
         return None
 
     def notify_observer(self, timestep):
-        while len(self.homes) and self.homes[-1][0] == self.simulation.steps:
-            timestep.attach(self.homes[-1][3].cars[0])
+        '''
+        Attaches all car agents to the timestep object once the current timestep is reached for
+        the car agents to start moving.
+
+        :param timestep: Timestep - reference to Timestep object
+        :return: None
+        '''
+        # attaches the car agent to Timestep object whenever it is time for the car to start moving
+        #   as specified by order.txt
+        # TODO: Consider using PriorityQueue
+        while len(self.homes) and self.homes[-1][TIMESTEP] == self.simulation.steps:
+            timestep.attach(self.homes[-1][HOME].cars[0])  # TODO: Avoid assuming only one Car agent in a Home
             self.homes.pop()
 
     def fix_state(self):
-        self.homes.sort(key=lambda x: x[0])
+        '''
+        Sorts all tuples in order and loads cars into Home objects.
+
+        This should be called before the simulations starts.
+
+        :return: None
+        '''
+        self.homes.sort(key=lambda x: x[0])  # TODO: potential error
         self.backup = self.homes
         self.fixed = True
 
@@ -38,12 +83,22 @@ class Waiting:
             h.load_car(desX, desY, self.pathfinder, hub)
 
     def reset(self):
+        '''
+        Resets the homes container to the original state.
+
+        :return: None
+        '''
         self.homes = self.backup
         for home in self.homes:
             h = home[3]
             h.clear_cars()
 
     def get_size(self):
+        '''
+        Gets the number of cars in waiting for convenience.
+
+        :return: Int - number of cars waiting
+        '''
         return len(self.homes)
 
     def __str__(self):
@@ -53,6 +108,3 @@ class Waiting:
             homes_list.append(home_string)
 
         return '\n'.join(homes_list)
-
-
-
