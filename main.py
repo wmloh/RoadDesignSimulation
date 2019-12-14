4f79c633635719a26c3b85bfe15d2d858c0e208b
Wei Min Loh <lohwm98@hotmail.com>
2019-12-14 14:38:09 -0500

    Expanded the configuration interface

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import numpy as np
 from configparser import ConfigParser
+from utils.config_converter import convert_parameters
 from interface.command_loop import CommandLoop
 from interface.training_simulation import TrainingSimulation
 
 if __name__ == '__main__':
-    config = ConfigParser()
+    # reads config.ini
+    config = ConfigParser(allow_no_value=True)
     config.read('config.ini')
+    config = convert_parameters(config)
 
+    # sets random seed
     np.random.seed(int(config['SIMULATION_TYPE']['random_seed']))
-    sim_type = config['SIMULATION_TYPE']['type']
 
+    sim_type = config['SIMULATION_TYPE']['type']
     if sim_type == 'cmd':
+        # command loop
         cmd = CommandLoop(**config['COMMAND_INIT_ARGS'])
         cmd.load_files(**config['COMMAND_LOAD_ARGS'])
         cmd.run()
 
     elif sim_type == 'train':
+        # training simulation
         train_sim = TrainingSimulation(**config['TRAIN_INIT_ARGS'])
-        train_sim.visualize_samples(3)
+
+        if config['TRAIN_CONTROL_FLOW']['visualize']:
+            train_sim.visualize_samples(**config['TRAIN_VISUALIZE'])
+
+        if config['TRAIN_CONTROL_FLOW']['generate']:
+            pass  # TODO: Incomplete
