6ec9239d4cf1b3b5483b809c7ec4d9097bcfb5e9
Wei Min Loh <lohwm98@hotmail.com>
2020-01-04 10:03:55 -0500

    Tile: Add method to convert to another tile while preserving invariants

--- tiles/tile.py
+++ tiles/tile.py
@@ -27,6 +27,27 @@ class Tile(ABC):
         '''
         self.neighbours.append(tile)
 
+    def convert_tile(self, constructor, ground):
+        '''
+        Converts this tile to another type while preserving invariants that are
+        independent of the tile type
+
+        :param constructor: (Int, Int, Int) => Traversable - constructor for a Traversable tile
+        :param ground: np.ndarray - reference to the simulation ground
+        :return: None
+        '''
+        constructed_tile = constructor(self.x, self.y, 1)
+        constructed_tile.neighbours = self.neighbours  # preserve neighbours
+
+        max_idx = len(self.neighbours) - 1
+        # updates affected neighbours
+        for idx, neighbour in enumerate(self.neighbours):
+            if neighbour:
+                neighbour.neighbours[max_idx - idx] = constructed_tile
+
+        # replaces current tile with the constructed tile
+        ground[self.y][self.x] = constructed_tile
+
     def get_coord(self):
         '''
         Returns the coordinates
